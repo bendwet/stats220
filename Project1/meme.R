163ca8b513fee5ea8ddf40761fc31b32e012fc75
bendwet <bendwet@outlook.com>
2024-03-10 13:12:48 +1300

    Project 1 gif and image project

---
+++ Project1/meme.R
@@ -0,0 +1,89 @@
+library(magick)
+
+# image 1
+average_hater <- image_read("images/hater.png") %>%
+  image_scale(500)
+
+# image 1 text
+hater_text <- image_blank(width = 500, 
+                          height = 50, 
+                          color = "#FFFFFF") %>%
+  image_annotate(text = "Typical Stats 220 Hater",
+                 color = "#000000",
+                 size = 40,
+                 font = "Arial",
+                 gravity = "center")
+
+
+# image 2
+average_enjoyer <- image_read("images/chad.jpg") %>%
+  image_scale(500)
+
+# image 2 text
+enjoyer_text <- image_blank(width = 500, 
+                            height = 50, 
+                            color = "#FFFFFF") %>%
+  image_annotate(text = "Average Stats 220 Enjoyer",
+                 color = "#000000",
+                 size = 40,
+                 font = "Arial",
+                 gravity = "center")
+
+# black bar
+text_black_bar <- image_blank(width = 3,
+                         height = 50,
+                         color = "#000000")
+image_black_bar <- image_blank(width = 3,
+                              height = 500,
+                              color = "#000000")
+
+# make the columns
+
+# text row
+text_vector <- c(hater_text, text_black_bar, enjoyer_text)
+top_row <- image_append(text_vector)
+
+# images row
+image_vector <- c(average_hater, image_black_bar, average_enjoyer)
+bottom_row <- image_append(image_vector)
+
+meme <- c(top_row, bottom_row) %>%
+  image_append(stack = TRUE)
+
+image_write(meme, "my_meme.png")
+
+# Gif part
+
+# numbers for the color squares text
+counts <- 1: 3 %>% as.character()
+
+green_square <- image_blank(color = 'green', width = 400, height = 400)
+orange_square <- image_blank(color = 'orange', width = 300, height = 300)
+red_square <- image_blank(color = 'red', width = 200, height = 200)
+
+frame1 <- red_square %>%
+  image_annotate(text = counts[3], size = 100, gravity = "center") %>%
+  image_scale(200) %>%
+  image_extent("500x500")
+
+frame2 <- orange_square %>%
+  image_annotate(text = counts[2], size = 100, gravity = "center") %>%
+  image_scale(300) %>%
+  image_extent("500x500")
+
+frame3 <- green_square %>%
+  image_annotate(text = counts[1], size = 100, gravity = "center") %>%
+  image_scale(400) %>%
+  image_extent("500x500")
+
+frame4 <- meme %>%
+  image_scale(500) %>%
+  image_extent("500x500")
+
+# frames vector
+frames <- c(frame1, frame2, frame3, frame4)
+
+# create animation
+animation <- image_animate(frames, fps = 1)
+
+image_write(animation, "my_animation.gif")
